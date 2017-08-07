83fa4dadef0094a5a753855c40ce4e5ffb99d671
kaizsv <kai.zsv@gmail.com>
2017-08-08 02:57:16 +0800

    bad

--- myex/Example_proof_final.thy
+++ myex/Example_proof_final.thy
@@ -34,6 +34,83 @@ lemma same_target_prop_proof':
   apply (rule iffI[rotated], simp)
   apply (clarsimp)
   done
+    
+theorem oghoare_composition_mergeI:
+  " Inv' \<parallel>-\<^sub>i Inv  p  c  q  \<Longrightarrow>
+       \<parallel>-\<^sub>i Inv' p' c'  q'  \<Longrightarrow> 
+    Inv''=Inv'\<inter>Inv \<Longrightarrow>
+    p''=p\<inter>p' \<Longrightarrow>
+    q''=q\<inter>q' \<Longrightarrow>
+    merge_prog_com c c' = Some c'' \<Longrightarrow>   
+       \<parallel>-\<^sub>i Inv'' p'' c'' q''"
+ by (simp add: oghoare_composition_merge)
+    
+lemma oghoare_inv_IntI: 
+  assumes Inv: "\<parallel>-\<^sub>i Inv p c q"
+  and Inv': "\<parallel>-\<^sub>i Inv' p c' q"
+  and merge: " merge_prog_com c c' = Some c''"
+  shows "\<parallel>-\<^sub>i (Inv \<inter> Inv') p c'' q"
+  apply (rule oghoare_composition_mergeI)
+       apply (rule Inv[THEN oghoare_inv_strengthen_assm])
+      apply clarsimp
+      apply (rule Inv')
+     apply blast+
+  apply (rule merge)
+  done
+    
+lemma same_prog_ann_com_refl:
+  "uses_all_pres c \<Longrightarrow> same_prog_ann_com c c"
+  by (induct c) auto
+    
+lemma same_prog_list_aux_refl_helper:
+  "\<forall>i<length (a # Ts).
+           (\<exists>y. OG_Tran.com ((a # Ts) ! i) = Some y) \<longrightarrow>
+           uses_all_pres (the (OG_Tran.com ((a # Ts) ! i))) \<Longrightarrow>
+   \<forall>i<length Ts.
+           (\<exists>y. OG_Tran.com (Ts ! i) = Some y) \<longrightarrow>
+           uses_all_pres (the (OG_Tran.com (Ts ! i)))"
+  by auto
+    
+lemma same_prog_list_aux_refl:
+  "(\<forall>i<length Ts. (\<exists>y. OG_Tran.com(Ts ! i) = Some y) \<longrightarrow> uses_all_pres (the(OG_Tran.com(Ts ! i))))
+    \<Longrightarrow> same_prog_list_aux Ts Ts"
+  apply (simp add: same_prog_list_aux_def same_prog_aux_def)
+  apply (induct Ts, simp)
+  apply (simp add: same_prog_list_aux_refl_helper)
+  apply (drule_tac x=0 in spec)
+  apply (clarsimp split: option.splits)
+  apply (clarsimp simp: same_prog_ann_com_refl split: option.splits)
+  done
+    
+lemma same_prog_com_refl:
+  "uses_all_pres' c \<Longrightarrow> same_prog_com c c"
+  apply (induct c)
+  apply (auto simp: same_prog_list_aux_refl)
+  done
+    
+lemma merge_ann_com_refl:
+  "uses_all_pres c \<Longrightarrow> merge_ann_com c c = Some c"
+  apply (induct c)
+  by auto
+    
+lemma merge_prog_list_aux_refl:
+  "\<forall>i<length Ts.
+              (\<exists>y. OG_Tran.com (Ts ! i) = Some y) \<longrightarrow>
+              uses_all_pres (the (OG_Tran.com (Ts ! i)))
+    \<Longrightarrow> merge_prog_list_aux Ts Ts = Some Ts"
+  apply (simp add: merge_prog_list_aux_def merge_prog_aux_def)
+  apply (induct Ts, simp)
+  apply (simp add: same_prog_list_aux_refl_helper)
+  apply (drule_tac x=0 in spec)
+  apply (clarsimp split: option.splits)
+  apply (auto simp: merge_ann_com_refl split: option.splits)
+  done
+    
+lemma merge_prog_com_refl:
+  "uses_all_pres' c \<Longrightarrow> merge_prog_com c c = Some c"
+  apply (induct c)
+  apply (auto simp: merge_prog_list_aux_refl split: option.splits)
+  done
   
 lemma will_not_fail_merge_same_prog_com: 
   "same_prog_com cs cs' \<Longrightarrow> merge_prog_com cs cs' = Some (the (merge_prog_com cs cs'))"
@@ -86,26 +163,98 @@ lemma same_prog_merge:
      apply (erule same_prog_com.elims; fastforce split: option.splits)+
   done
     
-    
-schematic_goal all_all_helper[simplified]:
-  "\<parallel>-\<^sub>i \<lbrace>True\<rbrace> ?p (?c :: 'a state_scheme com) ?q"
-  apply (rule oghoare_inv_cong')
-   apply (rule oghoare_composition_merge)
-    
 lemma same_prog_all:
   "same_prog_com same_target same_target"
   unfolding same_target_defs same_target''_def
   by auto
     
+lemma uses_all_pres_same_target:
+  "uses_all_pres' same_target"
+  unfolding same_target_defs same_target''_def
+  apply clarsimp
+  done
+    
+schematic_goal qq'[simplified]:
+  "\<parallel>-\<^sub>i \<lbrace>True\<rbrace> ?p (?c :: 'a state_scheme com) ?q"
+  apply (rule oghoare_inv_cong')
+   apply (rule oghoare_composition_merge)
+     apply (rule same_target_prop_proof)
+    apply (rule oghoare_inv_cong')
+     apply (rule oghoare_composition_merge)
+    
+       apply (rule same_target_prop_proof)
+    
+    
+    (*
+       apply (rule same_target_prop_proof[simplified])
+    apply (rule same_target_prop_proof[simplified])+
+     apply (rule will_not_fail_merge_same_prog_com)
+     apply (rule same_prog_com_refl)
+     apply (rule uses_all_pres_same_target)
+    apply clarsimp
+    (*apply (simp add: merge_prog_com_refl uses_all_pres_same_target)*)
+   apply (rule will_not_fail_merge_same_prog_com)
+    apply (simp add: merge_prog_com_refl uses_all_pres_same_target)
+   apply (rule same_prog_all)
+    apply (subst Collect_conj_eq_rev)+
+  apply (rule Collect_cong)
+  apply (fastforce )
+*)
+    done
+
+    
+    
+schematic_goal qq[simplified]:
+  "\<parallel>-\<^sub>i \<lbrace>True\<rbrace> ?p (?c :: 'a state_scheme com) ?q"
+  apply (rule oghoare_inv_cong')
+   apply (rule oghoare_composition_merge)
+     apply (rule same_target_prop_proof)
+    apply (rule qq')
+   apply (rule will_not_fail_merge_same_prog_com)
+    apply (simp add: merge_prog_com_refl uses_all_pres_same_target)
+   apply (rule same_prog_all)
+  apply fastforce
+    done
+    
+    
+schematic_goal all_all_helper[simplified]:
+  "\<parallel>-\<^sub>i \<lbrace>True\<rbrace> ?p (?c :: 'a state_scheme com) ?q"
+  apply (rule oghoare_inv_cong')
+   apply (rule oghoare_composition_merge)
+      apply (rule same_target_prop_proof[THEN oghoare_inv_strengthen_assm,
+                   where J' = "\<lbrace>\<acute>J\<rbrace>" for J])
+    apply (rule oghoare_inv_cong')
+     apply (rule oghoare_inv_IntI)
+       apply (rule same_target_prop_proof[simplified]) (** [simplified] **)
+    apply (rule qq)
+     apply (rule will_not_fail_merge_same_prog_com)
+    apply (simp add: merge_prog_com_refl uses_all_pres_same_target)
+     apply (rule same_prog_all)
+    apply (subst Collect_conj_eq_rev)+
+    (*apply (rule Collect_cong)*)
+    apply (simp)
+    apply (rule UNIV_eq_I)
+    apply (clarsimp)
+    apply simp
+   apply (rule will_not_fail_merge_same_prog_com)
+    apply (simp add: merge_prog_com_refl uses_all_pres_same_target)
+   apply (rule same_prog_all)
+    apply (subst Collect_conj_eq_rev)+
+  apply (rule Collect_cong)
+  apply (simp)
+    done
+   
+    
 schematic_goal all_all[simplified]:
   "\<parallel>-\<^sub>i (\<lbrace>True\<rbrace> \<inter> \<lbrace>True\<rbrace>)
         ?p
-        (?c::'a state_scheme com)
+        (?c::'a state_scheme com) (* need to double check *)
         ?q"
   apply (rule oghoare_inv_cong')
-   apply (rule oghoare_composition_merge
-          [of "\<lbrace>True\<rbrace>" "\<lbrace>True\<rbrace>" "\<lbrace>True\<rbrace>" same_target "\<lbrace>True\<rbrace>"])
+   apply (rule oghoare_composition_merge)
      apply (rule same_target_prop_proof')
+    apply (rule all_all_helper)
+    
     oops
   
 end
